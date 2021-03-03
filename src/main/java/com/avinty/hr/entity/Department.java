8983e852bf0b518585ae45ecb337ac8e80cf3828
fodorpapbalazsdev <fodorpapbalazsdev@gmail.com>
2021-03-03 17:19:54 +0100

    map Employees to Departments and to back

--- src/main/java/com/avinty/hr/entity/Department.java
+++ src/main/java/com/avinty/hr/entity/Department.java
@@ -1,13 +1,19 @@
 package com.avinty.hr.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "departments")
 @Getter
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +21,12 @@ public class Department {
 
     @NotNull
     private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "manager_id", referencedColumnName = "id")
+    private Employee manager;
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.EAGER)
+    private Set<Employee> employees;
+
 }
--- src/main/java/com/avinty/hr/entity/Employee.java
+++ src/main/java/com/avinty/hr/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.avinty.hr.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 
@@ -8,6 +10,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "employees")
 @Getter
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,5 +26,9 @@ public class Employee {
 
     @JoinColumn(name = "full_name")
     private String fullName;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "dep_id", referencedColumnName = "id")
+    private Department department;
 
 }
