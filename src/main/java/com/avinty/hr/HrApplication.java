1ba25327506b762dc23d8f0ea19f20e5d23a1a73
fodorpapbalazsdev <fodorpapbalazsdev@gmail.com>
2021-03-04 18:41:12 +0100

    app listen on port 5000

--- src/main/java/com/avinty/hr/HrApplication.java
+++ src/main/java/com/avinty/hr/HrApplication.java
@@ -2,8 +2,10 @@ package com.avinty.hr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EmbeddedTomcatConfiguration.class)
 public class HrApplication {
 
 	public static void main(String[] args) {
